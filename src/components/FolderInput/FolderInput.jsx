a3d33723b6c52862ab61e70e9a5c670a41763ca2
Omar Faruque Tuhin <omartuhin@pathao.com>
2023-04-06 12:53:44 +0600

    shorten file path

--- src/components/FolderInput/FolderInput.jsx
+++ src/components/FolderInput/FolderInput.jsx
@@ -3,18 +3,25 @@ import React from "react";
 
 export function FolderInput(props) {
   const {fileInputs, addFileInput} = props;
-  return <div className="cp-sidebar-header">
-    {fileInputs.map((fileInput) => (
-      <input
-        key={fileInput.key}
-        type="file"
-        onChange={props.onChange}
-        webkitdirectory="true"
-      />
-    ))}
-    <button onClick={addFileInput}>Add another folder</button>
-    <button onClick={props.onClick}>Reset</button>
-  </div>;
+  return (
+    <div className="cp-flex cp-flex-col cp-mb-4">
+      {fileInputs.map((fileInput) => (
+        <input
+          key={fileInput.key}
+          type="file"
+          onChange={props.onChange}
+          webkitdirectory="true"
+          className="cp-mb-2"
+        />
+      ))}
+      <button className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-indigo-600 cp-text-white cp-font-semibold" onClick={addFileInput}>
+        Add another folder
+      </button>
+      <button className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-gray-300" onClick={props.onClick}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 FolderInput.propTypes = {
