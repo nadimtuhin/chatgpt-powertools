b6921b30e27d33b232dea9a74dfea246e733df44
nadimtuhin <nadimtuhin@gmail.com>
2023-04-06 12:53:44 +0600

    shorten file path

--- src/components/FileList.jsx
+++ src/components/FileList.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import PropTypes from "prop-types";
+import {shortenPath} from "../utils/shortenPath.jsx";
 
-export const FileList = ({filePaths, insertFiles}) => {
+export function FileList({filePaths, insertFiles}) {
   return (
-    <ul>
+    <ul className="cp-list-inside cp-list-disc cp-pl-4 cp-mb-4">
       {filePaths.map((key) => (
-        <li key={key}>
+        <li key={key} className="cp-flex cp-items-center cp-mb-2">
           <button
             type="button"
+            className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-indigo-600 cp-text-white cp-font-semibold"
             onClick={() => insertFiles([key])}
           >
-            insert
+            Insert
           </button>
-          {key}
+          <span className="cp-truncate">{shortenPath(key)}</span>
         </li>
       ))}
     </ul>
-  )
+  );
 }
+
+FileList.propTypes = {
+  filePaths: PropTypes.arrayOf(PropTypes.string),
+  insertFiles: PropTypes.any
+};
