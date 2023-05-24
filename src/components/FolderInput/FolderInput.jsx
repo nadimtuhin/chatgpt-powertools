cc0fd8a2f273efb6a7f9f9dafa196b2d50e10471
Omar Faruque Tuhin <omartuhin@pathao.com>
2023-05-24 17:20:51 +0600

    updated reset button color

--- src/components/FolderInput/FolderInput.jsx
+++ src/components/FolderInput/FolderInput.jsx
@@ -5,15 +5,17 @@ export function FolderInput(props) {
   const { fileInputs, addFileInput } = props;
   return (
     <div className="cp-flex cp-flex-col cp-mb-4">
-      {fileInputs.map((fileInput) => (
-        <input
-          key={fileInput.key}
-          type="file"
-          onChange={props.onChange}
-          webkitdirectory="true"
-          className="cp-mb-2"
-        />
-      ))}
+      <div className="cp-min-h-20">
+        {fileInputs.map((fileInput) => (
+          <input
+            key={fileInput.key}
+            type="file"
+            onChange={props.onChange}
+            webkitdirectory="true"
+            className="cp-mb-2"
+          />
+        ))}
+      </div>
       <button
         className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-indigo-600 cp-text-white cp-font-semibold"
         onClick={addFileInput}
@@ -21,7 +23,7 @@ export function FolderInput(props) {
         Add another folder
       </button>
       <button
-        className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-indigo-600 cp-text-white cp-font-semibold"
+        className="cp-rounded cp-py-2 cp-px-4 cp-mr-2 cp-bg-red-500 cp-text-white cp-font-semibold"
         onClick={props.onClick}
       >
         Reset
@@ -29,6 +31,7 @@ export function FolderInput(props) {
     </div>
   );
 }
+
 
 FolderInput.propTypes = {
   onChange: PropTypes.func,
